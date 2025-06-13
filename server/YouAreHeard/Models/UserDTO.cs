5c9e6346c6b5127111e1e7eddc43b499ed83956d
Phùng Đức Tiệp <tombin082005@gmail.com>
2025-06-13 14:13:46 +0700

    Sử dụng constant values cho DataAnnotation ErrorMessage trong UserDTO

--- server/YouAreHeard/Models/UserDTO.cs
+++ server/YouAreHeard/Models/UserDTO.cs
@@ -4,25 +4,29 @@ namespace YouAreHeard.Models
 {
     public class UserDTO
     {
+        private const string t1 = "Không được để trống!";
+        private const string t2 = "Không đúng định dạng!";
+        private const string t3 = "Phải có ít nhất 6 ký tự!";
+        private const string t4 = "Không được chứa số hoặc ký tự đặc biệt!";
         public int UserId { get; set; }
 
-        [Required(ErrorMessage = "Email không được để trống")]
-        [EmailAddress(ErrorMessage = "Email không đúng định dạng")]
+        [Required(ErrorMessage = t1)]
+        [EmailAddress(ErrorMessage = t2)]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Mật khẩu không được để trống")]
-        [MinLength(6, ErrorMessage = "Mật khẩu phải có ít nhất 6 ký tự")]
+        [Required(ErrorMessage = t1)]
+        [MinLength(6, ErrorMessage = t3)]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Tên không được để trống")]
-        [RegularExpression(@"^[\p{L}\s]*$", ErrorMessage = "Tên không được chứa số hoặc ký tự đặc biệt")]
+        [Required(ErrorMessage = t1)]
+        [RegularExpression(@"^[\p{L}\s]*$", ErrorMessage = t4)]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Ngày sinh không được để trống")]
+        [Required(ErrorMessage = t1)]
         public DateTime Dob { get; set; }
 
-        [Required(ErrorMessage = "Số điện thoại không được để trống")]
-        [Phone(ErrorMessage = "Số điện thoại không hợp lệ")]
+        [Required(ErrorMessage = t1)]
+        [Phone(ErrorMessage = t2)]
         public string Phone { get; set; }
 
         public int RoleId { get; set; }
