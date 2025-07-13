da84554faa949cd39a0246bd3cc46cf742eff23d
Acharné <maiquocanh2608@gmail.com>
2025-07-13 21:52:11 +0700

    feature(Navbar): Update NavBar component and styles

--- client/src/component/NavBar/NavBar.js
+++ client/src/component/NavBar/NavBar.js
@@ -12,6 +12,10 @@ import HomeIcon from '../../uploads/icon/home.png';
 import LogoText from '../../uploads/logo-text.png';
 import LogoPicture from '../../uploads/logo-picture.png';
 import UserIcon from '../../uploads/icon/user.png';
+import ProfileIcon from '../../uploads/icon/profile.png';
+import LogoutIcon from '../../uploads/icon/logout.png';
+import SigninIcon from '../../uploads/icon/signin.png';
+import LoginIcon from '../../uploads/icon/login.png';
 
 // Components
 import Icon from '../Icon/Icon';
@@ -29,10 +33,10 @@ function NavBar({ user, setReloadCookies }) {
     const t5 = 'Đăng ký';
     const t6 = 'Thông tin người dùng';
     const t7 = 'Đăng xuất';
+    const t8 = 'Hồ sơ cá nhân';
 
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(null);
-    const [menuOpen, setMenuOpen] = useState(true);
     const navigate = useNavigate();
 
     const {
@@ -42,69 +46,70 @@ function NavBar({ user, setReloadCookies }) {
 
     return (
         <div className='nav-bar'>
-            <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
-                ☰
-            </button>
+            <div className='nav-bar-detail'>
+                <div className='logo'>
+                    <img src={LogoPicture} alt='logo-picture' />
+                    <img src={LogoText} alt='logo-text' />
+                </div>
 
-            {menuOpen && (
-                <div className='nav-bar-detail'>
-                    <div className='logo'>
-                        <img src={LogoPicture} alt='logo-picture' />
-                        <img src={LogoText} alt='logo-text' />
+                <div className='navigation'>
+                    <div onClick={() => navigate('/homePage')} className='home item'>
+                        <Icon src={HomeIcon} alt={'home-icon'} />
+                            <span>&nbsp;&nbsp;&nbsp;{t1}</span>
                     </div>
 
-                    <div className='navigation'>
-                        <div onClick={() => navigate('/homePage')} className='home'>
-                            <Icon src={HomeIcon} alt={'home-icon'} />
-                            &nbsp;&nbsp;&nbsp;{t1}
-                        </div>
-
-                        {user && (
-                            <div onClick={() => navigate('/infoPage')} className='info'>
-                                <Icon src={UserIcon} alt={'info-icon'} />
-                                &nbsp;&nbsp;&nbsp;{t6}
-                            </div>
-                        )}
-
-                        <div onClick={() => navigate('/blogPage')} className='blog'>
-                            <Icon src={BookIcon} alt={'blog-icon'} />
-                            &nbsp;&nbsp;&nbsp;{t2}
-                        </div>
-
-                        {user && (
-                            <div onClick={() => navigate('/appointmentPage')} className='appointment'>
-                                <Icon src={AppointmentIcon} alt={'appointment-icon'} />
-                                &nbsp;&nbsp;&nbsp;{t3}
-                            </div>
-                        )}
-                    </div>
-
-                    {user ? (
-                        <div className='authentication'>
-                            <button onClick={() => logout()} className='logout'>
-                                {t7}
-                            </button>
-                        </div>
-                    ) : (
-                        <div className='authentication'>
-                            <button onClick={() => navigate('/login')} className='login'>
-                                {t4}
-                            </button>
-                            <button onClick={() => navigate('/register')} className='register'>
-                                {t5}
-                            </button>
+                    {user && (
+                        <div onClick={() => navigate('/infoPage')} className='info item'>
+                            <Icon src={UserIcon} alt={'info-icon'} />
+                            <span>&nbsp;&nbsp;&nbsp;{t6}</span>
                         </div>
                     )}
 
-                    {loading && (
-                        <SkeletonUI />
-                    )}
+                    <div onClick={() => navigate('/blogPage')} className='blog item'>
+                        <Icon src={BookIcon} alt={'blog-icon'} />
+                            <span>&nbsp;&nbsp;&nbsp;{t2}</span>
+                    </div>
 
-                    {error && (
-                        <ErrorBox error={error} setError={setError} />
+                    {user && (
+                        <div onClick={() => navigate('/appointmentPage')} className='appointment item'>
+                            <Icon src={AppointmentIcon} alt={'appointment-icon'} />
+                            <span>&nbsp;&nbsp;&nbsp;{t3}</span>
+                        </div>
                     )}
                 </div>
-            )}
+
+                {user ? (
+                    <div className='authentication'>
+                        <button onClick={() => navigate('/patientProfile')} className='profile item'>
+                            <Icon src={ProfileIcon} alt={'profile-icon'} />
+                            <span>{t8}</span>
+                        </button>
+                        <button onClick={() => logout()} className='logout item'>
+                            <Icon src={LogoutIcon} alt={'logout-icon'} />
+                            <span>{t7}</span>
+                        </button>
+                    </div>
+                ) : (
+                    <div className='authentication non-login'>
+                        <button onClick={() => navigate('/login')} className='login item'>
+                            <Icon src={LoginIcon} alt={'login-icon'} />
+                            <span>{t4}</span>
+                        </button>
+                        <button onClick={() => navigate('/register')} className='register item'>
+                            <Icon src={SigninIcon} alt={'signin-icon'} />
+                            <span>{t5}</span>
+                        </button>
+                    </div>
+                )}
+
+                {loading && (
+                    <SkeletonUI />
+                )}
+
+                {error && (
+                    <ErrorBox error={error} setError={setError} />
+                )}
+            </div>
         </div>
     )
 }
